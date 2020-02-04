b5abbbf3a33aa3eac695cead48a8f353c4f3b788
lcs-JZhang <XZhang@lcs.on.ca>
2020-02-04 15:17:42 -0500

    Finished

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -33,6 +33,107 @@ let brown = Color(hue: 38, saturation: 99, brightness: 38, alpha: 100)
 let lightRed = Color(hue: 10, saturation: 80, brightness: 80, alpha: 75)
 
 // Begin your solution here...
+// Background Color
+canvas.fillColor = yellow
+canvas.drawRectangle(at: Point(x: 0, y: 0), width: 400, height: 600)
+
+canvas.drawShapesWithBorders = false
+
+// Draw custom shapes - start with brown
+
+
+// Write a function to simplify the steps
+func oneFiftyTimes100(point1: Point, point2: Point, point3: Point, point4: Point){
+    
+    var pointArray : [Point] = []
+    pointArray.append(point1)
+    pointArray.append(point2)
+    pointArray.append(point3)
+    pointArray.append(point4)
+    canvas.drawCustomShape(with: pointArray)
+    
+}
+
+
+// Use loop to draw the top and bottom row of quadrilaterals
+canvas.fillColor = brown
+
+// First shape
+var points : [Point] = []
+points.append(Point(x: 0, y: 220))
+points.append(Point(x: 10, y: 220))
+points.append(Point(x: 57.5, y: 320))
+points.append(Point(x: 0, y: 320))
+canvas.drawCustomShape(with: points)
+
+
+// First shape
+points = []
+points.append(Point(x: 0, y: 50))
+points.append(Point(x: 10, y: 50))
+points.append(Point(x: 57.5, y: 150))
+points.append(Point(x: 0, y: 150))
+canvas.drawCustomShape(with: points)
+
+// Switch from colors
+for n in 0...1 {
+
+for y in 0...2 {
+    
+    
+    if y % 2 == 0 {
+        canvas.fillColor = purple
+    } else {
+        canvas.fillColor = brown
+    }
+
+// Drwa the shapes (Type switch from Int to Double), add 160 each time to get the coordinates
+    
+    
+    oneFiftyTimes100(point1: Point(x: 67.5+160.0*Double(y), y: 150+170*Double(n)), point2: Point(x: 217.5+160.0*Double(y), y: 150+170*Double(n)), point3: Point(x: 170+160.0*Double(y), y: 50+170*Double(n)), point4: Point(x: 20+160.0*Double(y), y: 50+170*Double(n)))
+    
+    
+    
+    
+}
+
+
+}
+
+
+
+
+
+// Use loop to draw red quadrilateral
+canvas.fillColor = lightRed
+oneFiftyTimes100(point1: Point(x: 0, y: 235), point2: Point(x: 60, y: 235), point3: Point(x: 15, y: 135), point4: Point(x: 0, y: 135))
+for n in 0...2 {
+    canvas.fillColor = lightRed
+    oneFiftyTimes100(point1: Point(x: 70+160*n, y: 235), point2: Point(x: 220+160*n, y: 235), point3: Point(x: 175+160*n, y: 135), point4: Point(x: 25+160*n, y: 135))
+}
+
+
+
+
+// Add text
+// Subhumans
+canvas.drawText(message: "subhumans", at: Point(x: 10, y: 325), size: 48)
+
+// Info #1
+canvas.drawText(message: "no age limit", at: Point(x: 15, y: 397), size: 10)
+canvas.drawText(message: "advance $6.50, door $7.50", at: Point(x: 15, y: 412), size: 10)
+canvas.drawText(message: "friday, april 13 1984", at: Point(x: 15, y: 427), size: 10)
+
+
+// Info #2
+canvas.drawText(message: "los angeles, california", at: Point(x: 150, y: 397), size: 10)
+canvas.drawText(message: "1801 s. grand ave.", at: Point(x: 150, y: 412), size: 10)
+canvas.drawText(message: "at olympic auditorium", at: Point(x: 150, y: 427), size: 10)
+
+// Info #3
+canvas.drawText(message: "new regime & red scare", at: Point(x: 275, y: 397), size: 10)
+canvas.drawText(message: "multi death corporations", at: Point(x: 275, y: 412), size: 10)
+canvas.drawText(message: "with the tourists", at: Point(x: 275, y: 427), size: 10)
 
 
 /*:
